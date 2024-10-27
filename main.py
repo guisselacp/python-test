ee654fc3b73609db0bd3d9e9cebba2d0dc926ccf
Guissela Chumbe <guisselacp@gmail.com>
2024-10-27 23:16:49 +0000

    Add Function

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-print ("Hello world!")
 
-print("Please", "say", "hello", "back!")
-print(2+2)
-print(5+16)
-age = 17
-if age <= 18:
-    raise SystemExit('You must be older than 18!')
 
 # Declaring Variables in Python 
-
 num = 42
 print(num)
 temporary_password = True
 print(temporary_password)
+
+# Defining Function
+def print_message():
+    print("Hello World!")
+
+print_message()
