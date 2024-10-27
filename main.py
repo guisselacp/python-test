128a6aa5a5e5a3ca82f146bf62887c9a6421aa29
Guissela Chumbe <guisselacp@gmail.com>
2024-10-27 22:58:51 +0000

    Add Declaring Variables

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello world!")
+print ("Hello world!")
 
 print("Please", "say", "hello", "back!")
 print(2+2)
@@ -6,3 +6,10 @@ print(5+16)
 age = 17
 if age <= 18:
     raise SystemExit('You must be older than 18!')
+
+# Declaring Variables in Python 
+
+num = 42
+print(num)
+temporary_password = True
+print(temporary_password)
