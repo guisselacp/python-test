38fa5592497b332da54becc9c05f2bdda5d64ae6
Guissela Chumbe <guisselacp@gmail.com>
2024-10-29 10:48:36 +0000

    Add logical-operators.py

---
+++ logical-operators.py
@@ -0,0 +1,16 @@
+#Determining Truth With Logical Operators Challenge
+print(True and True)
+print(True and False)
+print(True or False)
+print(not (4 < 5 and 4 < 10))
+
+#Challenge
+a = 10
+b = 5
+
+result_one =(a > b)  and (a > 10)
+result_two = ((a == 5) or (b < 5))
+result_three = (not (result_two))
+print(result_one)
+print(result_two)
+print(result_three)
