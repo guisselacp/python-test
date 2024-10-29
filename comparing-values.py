b157388d789308c4a3378a0b44503434a9736c70
Guissela Chumbe <guisselacp@gmail.com>
2024-10-29 09:51:54 +0000

    Add string-methods.py

---
+++ comparing-values.py
@@ -0,0 +1,16 @@
+#Comparing Values With Comparison Operators
+print('Hello, World!' == 'Hello, World!')
+print(2!=2)
+print([1,2]<[1,2,3])
+print(float(2)>=int(2))
+print('a'<'A') #This is False as 'a' is Unicode 97 where 'A' is 65
+
+#Challenge
+a = 10
+b = 5
+
+print(a == b)
+print(a < b)
+print(a <= b)
+print(a > b)
+print(a >= b)
