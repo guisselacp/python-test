daffe2390938103c3d1a0900bed48ee6466b16a8
Guissela Chumbe <guisselacp@gmail.com>
2024-10-29 23:18:48 +0000

    Add ternary-operator.py

---
+++ ternary-operator.py
@@ -0,0 +1,11 @@
+my_boolean = False
+my_string = "Hello" if my_boolean else "World"
+
+print(my_string)
+
+#Challenge
+full_access = True
+result = None
+result= True if full_access else False
+
+print(result)
