b44dcabb86e7110d98aa1bfdba2623ccd65eb0ce
Guissela Chumbe <guisselacp@gmail.com>
2024-10-29 22:54:32 +0000

    Add if-else-statement.py

---
+++ if-else-statement.py
@@ -0,0 +1,20 @@
+number = int(input("Please enter a number:"))
+
+if number == 5:
+    print(f"{number} is equal to 5")
+else:
+    print(f"{number} is not equal to 5")
+
+#Challenge
+a = 10
+b = 20
+result = None
+
+# Write your if statement here:
+if a == b:
+   result= "a has the same value as b"
+   print(result)
+else:
+   result= "a has not got the same value as b"
+   print(result)
+    
