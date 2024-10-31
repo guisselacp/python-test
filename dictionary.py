fc8ba8e461f93b3c93e8221800463f727f7fcb6f
Guissela Chumbe <guisselacp@gmail.com>
2024-10-31 14:32:01 +0000

    Add dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+user = {
+    "username": "tombombadil",
+    "first_name": "Tom",
+    "last_name": "Bombadil",
+    "age": 100
+}
+
+print(user)
+print(user['age'])
+user['home'] = 'Withywindle, Middle-Earth'
+user['age'] = 99
+print(user)
+del user['home'] 
+print(user)
+print(list(user))
+print(sorted(user))
+print(user)
+print('username' in user)
+
+# Challenge
+spaceship = {
+    "name": "Red Dwarf",
+    "type": "Mining vessel",
+    "owner": "Jupiter Mining Corporation",
+    "captain": "Frank Hollister"
+}
+print(spaceship)
